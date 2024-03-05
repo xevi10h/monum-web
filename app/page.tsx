34ed7654762a030b6fb34e0d251d88c157feee6c
Pau Vilella <pau.vilella.st@gmail.com>
2024-03-05 15:36:51 +0100

    Fix typo

--- app/page.tsx
+++ app/page.tsx
@@ -22,7 +22,7 @@ export default function Page() {
             href="/login"
             className="flex items-center gap-5 self-start rounded-lg bg-green-500 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-green-400 md:text-base"
           >
-            <span>Accedir</span> <ArrowRightIcon className="w-5 md:w-6" />
+            <span>Accedir-hi</span> <ArrowRightIcon className="w-5 md:w-6" />
           </Link>
         </div>
         <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
